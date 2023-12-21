3dcf8a9edd739883b94dece2ad1c0b0081f15c09
Matteo <ezmatteo@aol.com>
2023-12-21 11:18:12 +0100

    prova di fetch con axios

--- src/App.js
+++ src/App.js
@@ -1,25 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css"
+import React from "react"
+import axios from "axios"
+import UsersTable from "./component/UsersTable"
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props)
+    console.log("constructor")
+    this.state = { users: [] }
+  }
+
+  componentDidMount() {
+    console.log("componentDidMount")
+    axios.get("https://jsonplaceholder.typicode.com/users").then((res) => {
+      const utenti = res.data
+      this.setState({ users: utenti })
+    })
+  }
+
+  render() {
+    console.log("render")
+    return (
+      <div className="App">
+        <header className="App-header">
+          <h1>React: CRUD e componentDidMount</h1>
+        </header>
+        <UsersTable users={this.state.users} />
+      </div>
+    )
+  }
 }
 
-export default App;
+export default App
